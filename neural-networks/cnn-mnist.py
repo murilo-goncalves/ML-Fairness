cb67e9fc205e5b191d6d49996067c3372f664caa
Murilo-Goncalves <murilogoncalves127@gmail.com>
2020-12-16 11:23:03 -0300

    fizemos uma super rede neural convolucional

--- neural-networks/cnn-mnist.py
+++ neural-networks/cnn-mnist.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
 from torch.autograd import Variable
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
-from torch.optim import SGD
+import pandas as pd
+
 
 mndata = MNIST('data')
 
@@ -14,31 +17,89 @@ np.set_printoptions(precision=2, suppress=True)
 
 X_train, y_train = mndata.load_training()
 X_test, y_test = mndata.load_testing()
-X_train, y_train = torch.from_numpy(np.array(X_train)/255), torch.from_numpy(np.array(y_train))
-X_test, y_test = torch.from_numpy(np.array(X_test)/255), torch.from_numpy(np.array(y_test))
+X_train, y_train = np.array(X_train)/255, np.array(y_train)
+X_test, y_test = np.array(X_test)/255, np.array(y_test)
 
-class CNN(Module):   
+BATCH_SIZE = 32
+
+torch_X_train = torch.from_numpy(X_train).type(torch.LongTensor)
+torch_y_train = torch.from_numpy(y_train).type(torch.LongTensor) # data type is long
+
+# create feature and targets tensor for test set.
+torch_X_test = torch.from_numpy(X_test).type(torch.LongTensor)
+torch_y_test = torch.from_numpy(y_test).type(torch.LongTensor) # data type is long
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(torch_X_train,torch_y_train)
+test = torch.utils.data.TensorDataset(torch_X_test,torch_y_test)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = BATCH_SIZE, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = BATCH_SIZE, shuffle = False)
+
+torch_X_train = torch_X_train.view(-1, 1,28,28).float()
+torch_X_test = torch_X_test.view(-1, 1,28,28).float()
+print(torch_X_train.shape)
+print(torch_X_test.shape)
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(torch_X_train,torch_y_train)
+test = torch.utils.data.TensorDataset(torch_X_test,torch_y_test)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = BATCH_SIZE, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = BATCH_SIZE, shuffle = False)
+
+def fit(model, train_loader):
+    optimizer = torch.optim.Adam(model.parameters())#,lr=0.001, betas=(0.9,0.999))
+    error = nn.CrossEntropyLoss()
+    EPOCHS = 5
+    model.train()
+    for epoch in range(EPOCHS):
+        correct = 0
+        for batch_idx, (X_batch, y_batch) in enumerate(train_loader):
+            var_X_batch = Variable(X_batch).float()
+            var_y_batch = Variable(y_batch)
+            optimizer.zero_grad()
+            output = model(var_X_batch)
+            loss = error(output, var_y_batch)
+            loss.backward()
+            optimizer.step()
+
+            # Total correct predictions
+            predicted = torch.max(output.data, 1)[1] 
+            correct += (predicted == var_y_batch).sum()
+            #print(correct)
+            if ((batch_idx) % 50 == 0):
+                print('Epoch : {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t Accuracy:{:.3f}%'.format(
+                    epoch, batch_idx*len(X_batch), len(train_loader.dataset), 100.*batch_idx / len(train_loader), loss.data, float(correct*100) / float(BATCH_SIZE*(batch_idx+1))))
+
+class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
+        self.conv3 = nn.Conv2d(32,64, kernel_size=5)
+        self.fc1 = nn.Linear(3*3*64, 256)
+        self.fc2 = nn.Linear(256, 10)
 
-        self.cnn_layers = Sequential(
-            # Defining a 2D convolution layer
-            Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
-            BatchNorm2d(4),
-            ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.linear_layers = Sequential(
-            Softmax(4 * 7 * 7, 10)
-        )
-
-    # Defining the forward pass    
     def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
+        x = F.relu(self.conv1(x))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(F.max_pool2d(self.conv3(x),2))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = x.view(-1,3*3*64 )
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
-model = CNN()
-optmizer = SGD(model.parameters(), lr=0.07)
+cnn = CNN()
+print(cnn)
+
+it = iter(train_loader)
+X_batch, y_batch = next(it)
+print(cnn.forward(X_batch).shape)
+
+fit(cnn,train_loader)

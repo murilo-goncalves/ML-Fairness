0cd54e11f52197bb70a94484c6ff5364685b8108
Murilo Gonçalves <murilogoncalves127@gmail.com>
2021-04-21 21:28:45 -0300

    started adult dataset analysis

--- classification/metrics/adults_dataset/adults_class.py
+++ classification/metrics/adults_dataset/adults_class.py
@@ -7,7 +7,6 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from yellowbrick.classifier import ClassificationReport, ConfusionMatrix, PrecisionRecallCurve
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -35,11 +34,11 @@ df_adults.columns = ['age',
 categorical = ['education', 'workclass', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'earnings']
 numerical = ['education-num', 'capital-gain', 'capital-loss', 'hours-per']
 
-# sns.pairplot(data=df_adults, hue='earnings', palette='gist_heat', dropna=True)
-# sns.boxplot(data=df_adults, x='earnings', y='age')
+sns.pairplot(data=df_adults, hue='earnings', palette='gist_heat', dropna=True)
+sns.boxplot(data=df_adults, x='earnings', y='age')
 sns.countplot(x='earnings', data=df_adults)
-# df_adults[numerical].hist()
-# plt.show()
+df_adults[numerical].hist()
+plt.show()
 
 le = preprocessing.LabelEncoder()
 for i in range(9):
@@ -47,7 +46,7 @@ for i in range(9):
 
 # print(df_adults.head())
 
-cols = [col for col in df_adults.columns if col is not "earnings"]
+cols = [col for col in df_adults.columns if col != "earnings"]
 data = df_adults[cols]
 target = df_adults['earnings']
 data_train, data_test, target_train, target_test = train_test_split(data, target, test_size = 0.20, random_state = 10)
--- compas/compas.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df_compas = pd.read_csv("archive/compas-scores-raw.csv")
-columns = df.columns
-
-sns.countplot(...)
